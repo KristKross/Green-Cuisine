5b1b17e9467b87d2d04560f040132ea50736871e
Kristian Demonteverde <kristkross30@gmail.com>
2025-03-22 03:53:59 +0400

    added sql pool

--- index.js
+++ index.js
@@ -15,10 +15,10 @@ const connection = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: 3306, // Optional, default is 3306
-    waitForConnections: true, // Optional, enables queueing
-    connectionLimit: 10, // Optional, defines pool size
-    queueLimit: 0 // Optional, no limit by default
+    port: 3306, 
+    waitForConnections: true, 
+    connectionLimit: 10,
+    queueLimit: 0 
 });
 
 const sessionStore = new MySQLStore({}, connection);
