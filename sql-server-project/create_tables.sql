81adc18c903f5dbf30ed44161b5ef639f1333b01
Kristian Demonteverde <kristkross30@gmail.com>
2025-05-04 13:02:34 +0400

    fixed sql tables to reflect the names in the code

--- sql-server-project/create_tables.sql
+++ sql-server-project/create_tables.sql
@@ -1,14 +1,13 @@
 -- For Edamam API
 CREATE TABLE users (
     UserID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255),
-    username VARCHAR(255),
-    password VARCHAR(255)
+    Email VARCHAR(255),
+    Username VARCHAR(255),
+    Password VARCHAR(255)
 );
 
 CREATE TABLE favourites (
     UserID INT,
-    RecipeName VARCHAR(100),
     RecipeURI VARCHAR(200),
     FOREIGN KEY (UserID) REFERENCES users(UserID)
 );
